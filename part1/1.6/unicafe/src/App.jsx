2c0fd1c14b939a9d8ad9c18549de76693cb2ac03
Casimir Lundberg <clundber@student.hive.fi>
2025-04-04 13:18:54 +0300

    1.7 done

--- part1/1.6/unicafe/src/App.jsx
+++ part1/1.6/unicafe/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+//generic button function
 const Button = (props) => (
   <button onClick={props.onClick}>
     {props.text}
@@ -11,17 +12,22 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
+  // handles the clicks
   const handleGoodClick = () => {
     setGood(good + 1)
+    setTotal(total +1)
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setTotal(total +1)
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setTotal(total +1)
   }
 
   return (
@@ -34,6 +40,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {total}</div>
+      <div>average {(good - bad) /total}</div>
+      <div>positive {(good / total) * 100} %</div>
     </div>
   )
 }
