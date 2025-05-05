40a2a5b12507d347beb6b608af9f4ff72a3c2950
Casimir Lundberg <clundber@student.hive.fi>
2025-05-05 16:16:59 +0300

    2.13 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
-import axios from 'axios'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   useEffect(() => {
-    axios.get('http://localhost:3001/persons').then(response => {
-      setPersons(response.data)
+    personService.getAll()
+    .then(initialPersons => {
+      setPersons(initialPersons)
     })
   }, [])
   const [newName, setNewName] = useState('')
@@ -40,9 +41,10 @@ const App = () => {
         alert(`${newName} is already added to phonebook`)
         return;
       }
-      axios.post('http://localhost:3001/persons', personObject)
-      .then(response => {
-      setPersons(persons.concat(response.data))
+      personService
+      .create(personObject)
+      .then(returnedPerson=> {
+      setPersons(persons.concat(returnedPerson))
       setNewName('')
       setNewNumber('')
     })}
