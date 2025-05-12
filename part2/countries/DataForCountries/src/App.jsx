e9e93169c549d7af6f1b2ac49246f858d81b067c
Casimir Lundberg <clundber@student.hive.fi>
2025-05-12 14:12:34 +0300

    2.18 done

--- part2/countries/DataForCountries/src/App.jsx
+++ part2/countries/DataForCountries/src/App.jsx
@@ -1,18 +1,49 @@
 import {useEffect, useState } from 'react'
 import countriesApi from './services/countries'
 
+const Country = ({country}) => {
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <div>Capital {country.capital}</div>
+      <div>Area {country.area}</div>
+      <h2>Languages</h2>
+      <ul>
+        {Object.entries(country.languages).map(([code, name]) =>
+          <li key={code}>{name}</li>
+        )}
+      </ul>
+      <img src={country.flags.png} alt={country.flags.alt}/>
+      {/* <button onClick={props.onClick}>show</button> */}
+    </div>
+  )
+}
 
-
-// const DisplayCountry = ({countries, filter}) => {
-//   const filtered = countries.filter(country => country === filter)
-
-//   return (
-//     <>
-//       {filtered.map()}
-    
-//     </>
-//   )
-// }
+const DisplayCountry = ({countries, filter}) => {
+  const filtered = countries.filter(country => 
+    country.name.common.toLowerCase().includes(filter.toLowerCase()))
+    // console.log(filtered)
+  if (filtered.length > 10) {
+    return( 
+      <div>Too many matches, specify another filter</div>
+    )
+  }
+  else if (filtered.length === 1) {
+    return (
+    <Country 
+    key={0}
+    country={filtered[0]}
+    />
+  )}
+  else {
+  return (
+      <div>
+        {filtered.map((country, index) =>
+        <div key={index}> {country.name.common} </div>
+        )}
+      </div>
+  )}
+}
 
 const App = () => {
   const [filter, setFilter] = useState('')
@@ -22,13 +53,12 @@ const App = () => {
     countriesApi.getAll()
     .then(apiCountries => {
      setCountries(apiCountries)
-     console.log(countries)
     })
   }, [])
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
-    console.log(event.target.value)
+    // console.log(event.target.value)
   }
 
 
@@ -46,7 +76,7 @@ const App = () => {
     <div>
       find countries
       <input placeholder='Country name' onChange={handleFilterChange}></input>
-      {/* <DisplayCountry countries={countries} filter={filter} /> */}
+      <DisplayCountry countries={countries} filter={filter} />
     </div>
   )
 }
