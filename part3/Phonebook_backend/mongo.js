64005a6bb91f88fe8804eff4b36d45bbcd67bd7d
Casimir Lundberg <clundber@student.hive.fi>
2025-05-20 15:25:23 +0300

    3.12 done

--- part3/Phonebook_backend/mongo.js
+++ part3/Phonebook_backend/mongo.js
@@ -4,6 +4,10 @@ if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
+else if (process.argv.length === 4 || process.argv.length > 5) {
+  console.log('input password name phonenumber')
+  process.exit(1)
+}
 
 const password = process.argv[2]
 
@@ -12,27 +16,39 @@ const url = `mongodb+srv://fullstack:${password}@cluster0.lrrqyck.mongodb.net/ph
 mongoose.set('strictQuery',false)
 
 mongoose.connect(url)
+  .then(() => {
+    console.log('connection successfull')
+  })
+  .catch((error) => {
+    console.error('Error in connecting to database', error.message)
+    process.exit(1)
+  })
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const contactSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Contact = mongoose.model('Contact', contactSchema)
 
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
+if ((process.argv.length == 5)) {
+const contact = new Contact({
+  name: process.argv[3],
+  number: process.argv[4],
 })
+  contact.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+    return ;
+  })
 
-Note.find({}).then(result => {
-	result.forEach(note => {
-	  console.log(note)
+}
+
+if (process.argv.length === 3){
+Contact.find({}).then(result => {
+	result.forEach(contact => {
+	  console.log(contact)
 	})
 	mongoose.connection.close()
   })
-
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
+  }
